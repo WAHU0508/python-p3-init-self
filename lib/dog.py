82780467485519156767e461a788f118ed52af00
WAHU0508 <sharongikenye@gmail.com>
2024-08-26 22:35:48 +0300

    Get all tests passing

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+fido = Dog("Fido")
+print(fido.name)
+print(fido.breed)
+snoopy = Dog("Snoopy", "Pitbull")
+print(snoopy.name)
+print(snoopy.breed)
